45fbc57d0227d4333d1e221d10318dd8a4dc5d67
Ícaro Magalhães <icaromagalhaes@lavid.ufpb.br>
2016-06-12 04:21:20 -0300

    refactor

--- bitonic-sort.cpp
+++ bitonic-sort.cpp
@@ -1,4 +1,16 @@
-#define __CL_ENABLE_EXCEPTIONS
+/*
+ * @Author: Ícaro Lima Magalhães
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <climits>
+#include <CL/cl.hpp>
+
 #define PROGRAM_FILE                "bitonic-sort.cl"
 #define BITONIC_SORT_INIT           "bitonic_sort_init"
 #define BITONIC_SORT_STAGE_0        "bitonic_sort_stage_zero"
@@ -6,29 +18,64 @@
 #define BITONIC_SORT_MERGE          "bitonic_sort_merge"
 #define BITONIC_SORT_MERGE_LAST     "bitonic_sort_merge_last"
 #define DIRECTION                   0
-#define DATA_SIZE                   32
-#define PRESENT_DATA_OUTPUT         false
+
+/*   POW(2, N) values to fill DATA_SIZE
+ *   2, 8, 32, 128, 512, 1024, 4096,
+ *   16384, 65536, 262144, 1048576
+ */
+#define DATA_SIZE 1048576
+
 #define PRESENT_DATA_INPUT          false
+#define PRESENT_DATA_OUTPUT         false
+#define PRESENT_PLATFORMS_DETAILS   true
+#define CALCULATE_EXECUTION_TIME    true
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <fstream>
-#include <random>
-#include <CL/cl.hpp>
+cl_int chech_integrity(const int *const _data);
 
-cl_int chech_integrity(int *_data);
+void present_data(const int *const _data)
+{
+    std::for_each(_data, _data + DATA_SIZE,
+        [&](int _elm){std::clog << _elm << std::endl;});
+    std::clog << std::endl;
+}
+
 void init_data(int *_data)
 {
     std::default_random_engine generator;
-    std::uniform_int_distribution<int> distribution(1,100);
+    std::uniform_int_distribution<const int> distribution(INT_MIN,INT_MAX);
 
-    for(int i = 0; i < DATA_SIZE; ++i)
+    std::generate(_data, _data + DATA_SIZE,
+        [&]() -> const int
+        {
+            return distribution(generator);
+        }
+    );
+    #if PRESENT_DATA_INPUT
+        present_data(_data);
+    #endif
+}
+
+void present_data_about_platforms(
+    const std::vector<cl::Device> &_platform_devices, std::vector<size_t> device_max_work_item_sizes)
+{
+    for(unsigned short i = 0; i < _platform_devices.size(); ++i)
     {
-        _data[i] = distribution(generator);
-        #if PRESENT_DATA_INPUT
-            std::cout << _data[i] << std::endl;
-        #endif
+        std::clog << _platform_devices[i].getInfo<CL_DEVICE_VENDOR>() << std::endl;
+        std::clog << _platform_devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
+        std::clog << "Max compute units: ";
+        std::clog << _platform_devices[i].getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl;
+        std::clog << "Max work item dimensions: ";
+        std::clog << _platform_devices[i].getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>() << std::endl;
+
+        device_max_work_item_sizes = _platform_devices[i].getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
+        std::clog << "Max work item sizes are: ";
+        for_each(device_max_work_item_sizes.begin(), device_max_work_item_sizes.end(),
+            [&](size_t _size)
+            {
+                std::clog << _size << ' ';
+            }
+        );
+        std::clog << std::endl;
     }
 }
 
@@ -46,36 +93,21 @@ int main(int argc, char const *argv[])
     std::vector<std::string> device_names;
     std::vector<size_t> device_max_work_item_sizes;
 
-    cl_int a = cl::Platform::get(&platforms);
+    std::chrono::steady_clock::time_point start, end;
+
+    cl::Platform::get(&platforms);
     platforms[0].getDevices(CL_DEVICE_TYPE_ALL, &platform_devices);
 
-    std::cout << "Your devices are: \n\n";
-    for(int i = 0; i < platform_devices.size(); ++i)
-    {
-        std::cout << platform_devices[i].getInfo<CL_DEVICE_VENDOR>() << std::endl;
-        std::cout << platform_devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
-        std::cout << "Max compute units: ";
-        std::cout << platform_devices[i].getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>() << std::endl;
-        std::cout << "Max work item dimensions: ";
-        std::cout << platform_devices[i].getInfo<CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS>() << std::endl;
-
-        device_max_work_item_sizes = platform_devices[i].getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
-        std::cout << "Max work item sizes are: ";
-        for_each(device_max_work_item_sizes.begin(), device_max_work_item_sizes.end(),
-            [&](size_t _size)
-            {
-                std::cout << _size << " ";
-            }
-        );
-        std::cout << "\n\n";
-    }
+    #if PRESENT_PLATFORMS_DETAILS
+        present_data_about_platforms(platform_devices, device_max_work_item_sizes);
+    #endif
 
     cl::Context context(platform_devices);
     ctx_devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
-    for(int i = 0; i < ctx_devices.size(); ++i)
-        std::cout << '[' << i << ']'
-            << ctx_devices[i].getInfo<CL_DEVICE_NAME>().c_str() << std::endl;
+    // for(int i = 0; i < ctx_devices.size(); ++i)
+    //     std::clog << '[' << i << ']'
+    //         << ctx_devices[i].getInfo<CL_DEVICE_NAME>().c_str() << std::endl;
 
     /* Open and build program */
     std::ifstream program_file(PROGRAM_FILE);
@@ -104,6 +136,9 @@ int main(int argc, char const *argv[])
     local_size = kernel_init.getWorkGroupInfo<CL_KERNEL_WORK_GROUP_SIZE>(ctx_devices[0]);
 
     local_size = (int)pow(2, trunc(log2(local_size)));
+
+    if(CALCULATE_EXECUTION_TIME) /*start calculating time*/
+        start = std::chrono::steady_clock::now();
 
     /* Create buffer */
     cl::Buffer data_buffer(context,
@@ -136,12 +171,8 @@ int main(int argc, char const *argv[])
 
         for(stage = high_stage; stage > 1; stage >>= 1)
         {
-
             kernel_stage_n.setArg(2, sizeof(int), &stage);
-
-            queue.enqueueNDRangeKernel(
-                kernel_stage_n, 1, global_size, local_size);
-
+            queue.enqueueNDRangeKernel( kernel_stage_n, 1, global_size, local_size);
         }
 
         queue.enqueueNDRangeKernel(
@@ -157,35 +188,33 @@ int main(int argc, char const *argv[])
     /* Perform the bitonic merge */
     for(stage = num_stages; stage > 1; stage >>= 1)
     {
-
         kernel_merge.setArg(2, sizeof(int), &stage);
-
-        queue.enqueueNDRangeKernel(
-            kernel_merge, 1, global_size, local_size);
-
+        queue.enqueueNDRangeKernel( kernel_merge, 1, global_size, local_size);
     }
 
-    queue.enqueueNDRangeKernel(
-        kernel_merge_last, 1, global_size, local_size);
+    queue.enqueueNDRangeKernel( kernel_merge_last, 1, global_size, local_size);
 
     /* Read the result */
     queue.enqueueReadBuffer(
-        data_buffer, CL_TRUE, 0, sizeof(data), data, NULL, NULL
-    );
+        data_buffer, CL_TRUE, 0, sizeof(data), data, NULL, NULL );
+
+    if(CALCULATE_EXECUTION_TIME)//end calculating time
+    {
+        end = std::chrono::steady_clock::now();
+        std::clog <<
+            std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()
+            << std::endl;
+    }
 
     if(chech_integrity(data))
     {
-        std::cout << "Success!" << std::endl;
-        if(PRESENT_DATA_OUTPUT)
-            for(int i = 0; i < DATA_SIZE; ++i)
-                std::cout << data[i] << std::endl;
+        std::clog << "Success!" << std::endl;
+        #if PRESENT_DATA_OUTPUT
+            present_data(data);
+        #endif
 
     }
-    else
-    {
-        std::cout << "Sotring failed." << std::endl;
-    }
-
+    else std::clog << "Sorting failed." << std::endl;
 
     return 0;
 
